301fcaf8ca63739caccc8a412e6f50e3f55b89e8
Aleksei Arro <alexey.arro@waytostart.ru>
2019-03-05 15:55:58 +0300

    CTIG project update: rev1

--- ctig/js/custom-scrollbars.js
+++ ctig/js/custom-scrollbars.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$(".product-list__wrapper").mCustomScrollbar({axis:"y",alwaysShowScrollbar:"1"})});
+$(document).ready(function(){$(".product-list__wrapper").mCustomScrollbar({axis:"y",alwaysShowScrollbar:"1"})}),$(document).ready(function(){window.innerWidth>=1200&&$(".navigation__list-wrapper").mCustomScrollbar({axis:"y",alwaysShowScrollbar:"0"})});
