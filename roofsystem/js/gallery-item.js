a0237574ff79b010b527bc09fcc2090edf046637
Aleksei Arro <alexey.arro@waytostart.ru>
2019-04-15 15:32:45 +0300

    Roofsystem project updated

--- roofsystem/js/gallery-item.js
+++ roofsystem/js/gallery-item.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){const e={autoplay:!0,arrows:!1,dots:!1,infinite:!0,slidesToShow:1,slidesToScroll:1,speed:1e3,fade:!0};for(let o=1;o<=4;o++)$(`.gallery-item--${o} .gallery-item__box--pictures`).slick(e)});
+$(document).ready(function(){const o={autoplay:!0,arrows:!1,dots:!1,infinite:!0,slidesToShow:1,slidesToScroll:1,speed:1e3,fade:!0};for(let e=1;e<=4;e++)$(`.gallery-item--${e} .gallery-item__box--pictures`).slick(o);lightbox.option({wrapAround:!0})});
